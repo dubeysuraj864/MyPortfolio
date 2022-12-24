fd077a6110090c6c59a53b4f8a78f9b61abc77ef
Suraj Dubey <dubeysuraj864@gmail.com>
2022-12-24 12:25:42 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home.jsx";
+// import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Service from "./pages/Service.jsx";
 import MyWork from "./pages/Mywork.jsx";
 import Footer from "./components/Footer";
+
+const Home = React.lazy(() => import("./pages/Home"));
 
 function App() {
   return (
@@ -12,11 +16,15 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route
-              path="/"
-              element={<Navbar className="z-30" />}
-            >
-              <Route index element={<Home />} />
+            <Route path="/" element={<Navbar className="z-30" />}>
+              <Route
+                index
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <Home />
+                  </Suspense>
+                }
+              />
               <Route path="/about" element={<About />} />
               <Route path="/service" element={<Service />} />
               <Route path="/mywork" element={<MyWork />} />
