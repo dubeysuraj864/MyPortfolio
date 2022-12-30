8458d541f0d74e08597bf2b731cc78611242c82d
Suraj Dubey <dubeysuraj864@gmail.com>
2022-12-30 19:43:25 +0530

    ui updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Linkedin from "../images/linkedin.webp";
 function Footer() {
   return (
     <>
-      <div className="footer flex flex-col justify-center pt-20 bg-green-100 px-2 rounded-t-2xl">
+      <div className="footer flex flex-col justify-center pt-20 bg-black px-2 rounded-t-2xl">
         <div className="flex-col justify-between items-center shadow-lg bg-white p-10 rounded-2xl w-fit ml-auto mr-auto mb-16 md:w-3/4 md:flex-row md:flex">
           <div className="left mr-10 ">
             <div className="text-2xl md:text-4xl md:font-bold my-2">Have any project in mind?</div>
@@ -17,11 +17,11 @@ function Footer() {
           </div>
         </div>
 
-        <div className=" flex flex-col md:flex md:flex-row md:justify-around pb-8 w-full justify-center items-center">
+        <div className=" flex flex-col md:flex md:flex-row md:justify-around pb-8 w-full justify-center items-center text-white">
           <div className=" mb-4 left">
             Designed and developed by Suraj Dubey 2022
           </div>
-          <div className="right flex justify-center cursor-pointer">
+          <div className="right flex justify-center cursor-pointer invert">
             <a target="_blank" href="https://www.instagram.com/dubeysuraj864/" rel="noreferrer">
               <img src={Instagram} width={20} className="mr-2" alt="" />
             </a>
